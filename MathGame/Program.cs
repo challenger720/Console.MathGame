f6c86831efd703b97e65e36a7bc050860a03ff33
challenger720 <127072307+challenger720@users.noreply.github.com>
2024-02-12 20:08:31 -0700

    initial program

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string? menuSelection;
+Console.WriteLine("What game would you like to play today? Choose from the options below:");
+Console.WriteLine("V - View Previous Games");
+Console.WriteLine("A - Addition");
+Console.WriteLine("S - Subtraction");
+Console.WriteLine("M - Multiplication");
+Console.WriteLine("D - Division");
+Console.WriteLine("Q - Quit the program");
+Console.WriteLine("----------------------------------------------------------------------");
+
+do
+{
+    menuSelection = Console.ReadLine();
+} while (menuSelection == null && (menuSelection != "V" || menuSelection != "A" || menuSelection != "S" || menuSelection != "M" || menuSelection != "D" || menuSelection != "Q"));
+
+Console.Clear();
+
+switch (menuSelection)
+{
+    case "V":
+        break;
+    case "A":
+        break;
+    case "S":
+        break;
+    case "M":
+        break;
+    case "D":
+        break;
+    case "Q":
+        break;
+}
