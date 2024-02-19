97600fc1eb0183cb9e66fd6df4fcbbfe7f9c3010
challenger720 <127072307+challenger720@users.noreply.github.com>
2024-02-18 21:00:44 -0700

    Added point history

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,20 +1,23 @@
-﻿string? menuSelection;
+using System.Reflection.Metadata.Ecma335;
+
+string? menuSelection;
 string? answer;
 Random random = new Random();
 int number;
-int point = 0;
+int point;
 bool verify;
 bool showMenu = true;
+var history = new List<string>();
 
 while (showMenu)
 {
+    Console.Clear();
     MainMenu();
     showMenu = MainMenu();
 }
 
 bool MainMenu()
 {
-    Console.Clear();
     Console.WriteLine("What game would you like to play today? Choose from the options below:");
     Console.WriteLine("V - View Previous Games");
     Console.WriteLine("A - Addition");
@@ -27,43 +30,57 @@ bool MainMenu()
     do
     {
         menuSelection = Console.ReadLine();
-    } while ((menuSelection == null) || (menuSelection != "V" || menuSelection != "A" || menuSelection != "S" || menuSelection != "M" || menuSelection != "D" || menuSelection != "Q"));
+    } while ((menuSelection == null) || (menuSelection != "V" && menuSelection != "A" && menuSelection != "S" && menuSelection != "M" && menuSelection != "D" && menuSelection != "Q"));
 
     switch (menuSelection)
     {
         case "V":
+            foreach (var log in history)
+            {
+                Console.WriteLine(log);
+            }
+            Console.WriteLine("Press any key to return to Main Menu.");
+            Console.ReadLine();
             return true;
         case "A":
+            point = 0;
             for (int i = 0; i < 5; i++)
             {
                 Console.Clear();
                 Addition();
             }
             Score();
+            history.Add($"Addition: {point}");
             return true;
         case "S":
+            point = 0;
             for (int i = 0; i < 5; i++)
             {
                 Console.Clear();
                 Subtraction();
             }
             Score();
+            history.Add($"Subtraction: {point}");
             return true;
         case "M":
+            point = 0;
             for (int i = 0; i < 5; i++)
             {
                 Console.Clear();
                 Multiply();
             }
             Score();
+            history.Add($"Multiplication: {point}");
             return true;
         case "D":
+            point = 0;
             for (int i = 0; i < 5; i++)
             {
                 Console.Clear();
                 Division();
             }
             Score();
+            history.Add($"Division: {point}");
             return true;
         case "Q":
             return false;
@@ -89,8 +106,13 @@ void Addition()
 
 void Subtraction()
 {
-    int A = random.Next(1, 101);
-    int B = random.Next(1, 101);
+    int A;
+    int B;
+    do
+    {
+        A = random.Next(1, 101);
+        B = random.Next(1, 101);
+    } while (A < B);
     int sub = A - B;
     Console.WriteLine("Subtraction game");
     Console.WriteLine($"{A} - {B}");
@@ -104,8 +126,8 @@ void Subtraction()
 
 void Multiply()
 {
-    int A = random.Next(1, 101);
-    int B = random.Next(1, 101);
+    int A = random.Next(1, 11);
+    int B = random.Next(1, 11);
     int product = A * B;
     Console.WriteLine("Multiplication game");
     Console.WriteLine($"{A} * {B}");
@@ -148,9 +170,11 @@ void Check(int input, int ans)
     }
     else
         Console.WriteLine("Incorrect. Type any key for next question");
+    Console.ReadLine();
 }
 
 void Score()
 {
     Console.WriteLine($"Game over. Your final score is {point}. Press any key to go back to main menu.");
+    Console.ReadLine();
 }
